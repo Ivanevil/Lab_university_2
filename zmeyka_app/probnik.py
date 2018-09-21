56a057ed33abe900dc6d91be05aa439b5b8f45e2
Andrey <mraero17@mail.ru>
2018-09-21 15:54:09 +0300

    1

--- zmeyka_app/probnik.py
+++ zmeyka_app/probnik.py
@@ -1,11 +1,23 @@
-l1 = [1]
-s = 1
+def f(x):
+    return x**2 - 4 * x - 18 - x**3
 
-while sum(l1[0:s]) - sum(l1[0:s - 1]) > 0.001:
-    s += 1
-    l1 = [216 / 7 * (i**2 + 8 * i + 15) for i in range(s)]
-l2 = [sum(l[0:k]) for k in range(s)]
 
-print(s)
-print(sum(l1))
-print(l2)
+eps = 0.0001
+
+
+def formula(a, b):
+    return f(a) - f(b) - 0.2 * (f(b)**2 + 2 * f(b) - 1)
+
+
+def itr(a, b, imax):
+    for i in range(imax):
+        a = formula(i, i + 1)
+        b = formula(i, i + 1)
+        print(b)
+        if abs(f(a) - f(b)) < eps:
+            break
+
+    return a, b
+
+
+print(itr(0, 2, 2000))
